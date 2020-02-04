4eec6fac7854c7cf0372527ffbab197ca3bb35bd
Sergey Fegon <sergey.fegon@websailors.pro>
2020-02-04 13:15:29 +0300

    simple request

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 5000;
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -11,7 +14,12 @@ app.get('/', function (req, res) {
 
 app.get('/users', (req, res) => {
   // Some code
-  res.send({});
+  res.send({
+    users: [{
+      username: 'John Doe',
+      birthday: new Date(1999, 1, 1).getTime()
+    }]
+  });
 })
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000'
+
 class App extends Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    axios.get(`${API_URL}/users`)
+      .then(result => {
+        if(result.data && result.data.users) {
+          this.setState({
+            users: result.data.users
+          })
+        }
+      })
+      .catch(error => {
+        console.log('getting users error', error)
+      })
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>User List</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-intro">
+          {this.state.users.map(user => (
+            <li>
+              {`${user.username} - ${new Date(user.birthday)}`}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
