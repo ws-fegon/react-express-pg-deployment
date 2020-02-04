07c7b46dd3da11b38c9664f0b415a23bff85f643
Sergey Fegon <sergey.fegon@websailors.pro>
2020-02-04 14:21:32 +0300

    added docker-compose.yml, run postgres

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 5000;
+
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_HOST = process.env.POSTGRES_HOST;
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
+  host: POSTGRES_PASSWORD,
+  dialect: 'postgres'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    require('./user-model')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 app.use(cors())
 
@@ -13,7 +36,6 @@ app.get('/', function (req, res) {
 
 
 app.get('/users', (req, res) => {
-  // Some code
   res.send({
     users: [{
       username: 'John Doe',
