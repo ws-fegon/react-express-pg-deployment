48b46e62609cf07c15e953ebf0a9ab1a85802e8d
Sergey Fegon <sergey.fegon@websailors.pro>
2020-02-11 20:04:35 +0300

    quick fix

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,13 +1,15 @@
 const db = require('../models/index');
 
 const userController = (req, res) => {
-  db.User.findAll()
-    .then(users => {
-      res.send({users})
-    })
-    .catch(err => {
-      res.status(500).send(err)
-    });
+  if (db && db.User) {
+    db.User.findAll()
+      .then(users => {
+        res.send({users})
+      })
+      .catch(err => {
+        res.status(500).send(err)
+      });
+  }
 }
 
 module.exports = userController;
